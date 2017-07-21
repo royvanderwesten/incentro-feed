e5f4de7914e5f256ed4d0248ae8c6519f39c55fe
Jason Koolman <jason.koolman@incentro.com>
2017-07-21 11:01:40 +0200

    Implement Google Maps service

--- src/asset/javascript/site.js
+++ src/asset/javascript/site.js
@@ -1,4 +1,16 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 
+window.loadMap = function() {
 
+    const latLng = {lat: -25.363, lng: 131.044};
 
+    const map = new google.maps.Map(document.querySelector('.app-maps'), {
+        zoom: 4,
+        center: latLng
+    });
+
+    const marker = new google.maps.Marker({
+        map: map,
+        position: latLng
+    });
+};
