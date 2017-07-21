d0e7890fe5ba59b682002766bc0c27a78bcb9222
Jason Koolman <jason.koolman@incentro.com>
2017-07-21 14:08:25 +0200

    Big update

--- src/asset/javascript/sidebar.component.js
+++ src/asset/javascript/sidebar.component.js
@@ -5,6 +5,7 @@ export class SidebarComponent {
 
     constructor(map) {
         this.map = map;
+        this.$sidebar = $('.app-sidebar');
     }
 
     /**
@@ -13,21 +14,45 @@ export class SidebarComponent {
      * @param {string} key
      */
     load(key) {
-        //let element = document.querySelector('.app-sidebar');
         let service = new google.maps.places.PlacesService(this.map);
 
         service.getDetails({
             placeId: LOCATIONS[key].placeId
         }, (place, status) => {
-            console.log(place, status);
+            this.renderTitle(LOCATIONS[key].title);
+            this.renderImage(place);
+            this.renderDetails(place);
         });
+    }
+
+    /**
+     * Render the sidebar's title.
+     *
+     * @param title
+     */
+    renderTitle(title) {
+        this.$sidebar.find('.app-sidebar__title').text(title);
+    }
+
+    /**
+     * Render the place image.
+     */
+    renderImage(place) {
+        const source = typeof place.photos[0] !== 'undefined' ?
+            place.photos[0].getUrl({maxWidth: 440, maxHeight: 240}):
+            'https://www.bonque.nl/uploads/plaatje_bij_over_incentro.png';
+
+        this.$sidebar.find('.app-sidebar__image').attr('src', source);
     }
 
     /**
      * Render the place details.
      */
-    renderDetails() {
-
+    renderDetails(place) {
+        this.$sidebar.find('.feedblock--intro').html(`
+            <p>${place.adr_address.substring(0,  place.adr_address.lastIndexOf(','))}</p>
+            <p>${place.formatted_phone_number}</p>
+        `);
     }
 
 }
--- src/asset/javascript/site.js
+++ src/asset/javascript/site.js
@@ -2,32 +2,32 @@ import { MapsComponent } from './maps.component';
 
 export const LOCATIONS = {
     am: {
-        title: 'Incentro Amsterdam',
+        title: 'Amsterdam',
         latLng: {lat: 52.3991827, lng: 4.871793099999991},
         placeId: 'ChIJVfMFwP1HZUERDfr8-t7kwFo'
     },
     ro: {
-        title: 'Incentro Rotterdam',
+        title: 'Rotterdam',
         latLng: {lat: 51.9232379, lng: 4.433187099999941},
         placeId: 'ChIJ7fq05ds0xEcRXYbm85-aL8Y'
     },
     dh: {
-        title: 'Incentro Den Haag (SI & DS),',
+        title: 'Den Haag (SI & DS),',
         latLng: {lat: 52.0408476, lng: 4.354507399999989},
         placeId: 'ChIJ7fq05ds0xEcRXYbm85-aL8Y'
     },
     mt: {
-        title: 'Incentro Utrecht (Marketing Technology)',
+        title: 'Utrecht (MT)',
         latLng: {lat: 52.10518800000001, lng: 5.080437999999958},
         placeId: 'ChIJw32okKFvxkcRAkQ0YylTtDo'
     },
     ba: {
-        title: 'Incentro Utrecht (Business Acceleration)',
+        title: 'Utrecht (BA)',
         latLng: {lat: 52.067187, lng: 5.086576100000002},
         placeId: 'ChIJ3xXflZtlxkcR7iiNFeMTfpk'
     },
     cd: {
-        title: 'Incentro Utrecht (Cloud & Digital Transformation)',
+        title: 'Utrecht (Cloud & DT)',
         latLng: {lat: 52.06528199999999, lng: 5.0901099999999815},
         placeId: 'ChIJyxV00EJlxkcR9_vDWm3TAYM'
     }
